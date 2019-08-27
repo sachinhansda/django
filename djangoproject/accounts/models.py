9ffd0093960626b42d896a7cef431d97b1add466
Sachin Hansda <sachinhansda8@gmail.com>
2019-08-27 14:14:25 +0530

    login and logout done

--- djangoproject/accounts/models.py
+++ djangoproject/accounts/models.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	decription = models.CharField(max_length=100, default='')
+	city = models.CharField(max_length=100, default='')
+	phone = models.IntegerField(default=0)
+
+def create_profile(sender, **kwargs):
+	if kwargs['created']:
+		user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
