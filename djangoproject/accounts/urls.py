db7bb30d7ebbc7994591c141850dea662f7d1f27
Sachin Hansda <sachinhansda8@gmail.com>
2019-08-29 11:47:28 +0530

    user profile

--- djangoproject/accounts/urls.py
+++ djangoproject/accounts/urls.py
@@ -5,5 +5,8 @@ from django.contrib.auth.views import login, logout
 urlpatterns = [
     url(r'^$', views.home),
     url(r'^login/$', login, {'template_name': 'accounts/login.html'}),
-    url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'})    
+    url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'}),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^profile/$', views.view_profile, name='view_profile'),
+    url(r'^profile/edit/$', views.edit_profile, name='edit_profile')
 ]
--- djangoproject/accounts/views.py
+++ djangoproject/accounts/views.py
@@ -1,12 +1,47 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect 
+from accounts.forms import RegistrationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm
 
 # Create your views here.
 def home(request):
-    numbers = [1,2,3,4,5]
-    name = 'Sachin Hansda'
+    	numbers = [1,2,3,4,5]
+    	name = 'Sachin Hansda'
 
-    args = {'myName': name, 'numbers': numbers}
-    return render(request, 'accounts/home.html', args)
+    	args = {'myName': name, 'numbers': numbers}
+    	return render(request, 'accounts/home.html', args)
+
+
+def register(request):
+	if request.method == 'POST':
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/account')
+	else:
+		form = RegistrationForm() 
+
+		args = {'form': form}
+		return render(request, 'accounts/reg_form.html', args)
+
+
+def view_profile(request):
+	args = {'user': request.user}
+	return render(request, 'accounts/profile.html', args)
+
+def edit_profile(request):
+	if request.method  == 'POST':
+		form = UserChangeForm(request.POST, instance=request.user)
+
+		if form.is_valid():
+			form.save()
+			return redirect('/account/profile')
+
+	else:
+		form = UserChangeForm(instance=request.user)
+		args = {'form': form}
+		return render(request, 'accounts/edit_profile.html', args)
+	
