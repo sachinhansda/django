0cc495b0dc6f7e848e19cd76565b15daa34f51fa
Sachin Hansda <sachinhansda8@gmail.com>
2019-09-07 18:39:38 +0530

    anonymous users are denied access to website

--- djangoproject/accounts/views.py
+++ djangoproject/accounts/views.py
@@ -9,8 +9,10 @@ from accounts.forms import (
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def home(request):
     	numbers = [1,2,3,4,5]
     	name = 'Sachin Hansda'
@@ -32,11 +34,12 @@ def register(request):
 		return render(request, 'accounts/reg_form.html', args)
 
 
+@login_required
 def view_profile(request):
 	args = {'user': request.user}
 	return render(request, 'accounts/profile.html', args)
 
-
+@login_required
 def edit_profile(request):
 	if request.method  == 'POST':
 		form = EditProfileForm(request.POST, instance=request.user)
@@ -50,7 +53,7 @@ def edit_profile(request):
 		args = {'form': form}
 		return render(request, 'accounts/edit_profile.html', args)
 
-
+@login_required
 def change_password(request):
 	if request.method == 'POST':
 		form = PasswordChangeForm(data=request.POST, user=request.user)
