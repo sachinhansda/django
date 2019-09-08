edf9d0a3725666ae394744b70b4767ce373424b6
Sachin Hansda <sachinhansda8@gmail.com>
2019-09-08 22:21:15 +0530

    process_view middleware

--- djangoproject/accounts/views.py
+++ djangoproject/accounts/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required
+
 def home(request):
     	numbers = [1,2,3,4,5]
     	name = 'Sachin Hansda'
@@ -34,12 +34,12 @@ def register(request):
 		return render(request, 'accounts/reg_form.html', args)
 
 
-@login_required
+
 def view_profile(request):
 	args = {'user': request.user}
 	return render(request, 'accounts/profile.html', args)
 
-@login_required
+
 def edit_profile(request):
 	if request.method  == 'POST':
 		form = EditProfileForm(request.POST, instance=request.user)
@@ -53,7 +53,7 @@ def edit_profile(request):
 		args = {'form': form}
 		return render(request, 'accounts/edit_profile.html', args)
 
-@login_required
+
 def change_password(request):
 	if request.method == 'POST':
 		form = PasswordChangeForm(data=request.POST, user=request.user)
