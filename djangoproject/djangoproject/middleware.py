336af357227df655ae278c3e5b9ebfe937044c37
Sachin Hansda <sachinhansda8@gmail.com>
2019-09-16 20:11:36 +0530

    avoid hardcoded urls

--- djangoproject/djangoproject/middleware.py
+++ djangoproject/djangoproject/middleware.py
@@ -1,6 +1,7 @@
 import re
 
 from django.conf import settings
+from django.urls import reverse
 from django.shortcuts import redirect
 from django.contrib.auth import logout
 
@@ -20,13 +21,11 @@ class LoginRequiredMiddleware:
 		assert hasattr(request, 'user')
 		path = request.path_info.lstrip('/')
 
-		# if not request.user.is_authenticated():
-		#	if not any(url.match(path) for url in EXEMPT_URLS):
-		#		return redirect(settings.LOGIN_URL) 
+		return redirect(settings.LOGIN_URL) 
 
 		url_is_exempt = any(url.match(path) for url in EXEMPT_URLS)
 
-		if path == 'account/logout/':
+		if path == reverse('logout').lstrip('/'):
 			logout(request)
 
 		if request.user.is_authenticated() and url_is_exempt:
