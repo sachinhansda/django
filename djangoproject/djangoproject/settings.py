c08b3c87fa9a26230ed5776f4473a9f9bb75ef33
Sachin Hansda <sachinhansda8@gmail.com>
2019-09-17 23:31:46 +0530

    Reset Password Feature Accessible for Logged Out Users Only

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -128,6 +128,10 @@ LOGIN_URL = '/account/login/'
 LOGIN_EXEMPT_URLS = (
 	r'^account/logout/$',
 	r'^account/register/$',
+	r'^account/reset-password/$',
+	r'^account/reset-password/done/$',
+	r'^account/reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+	r'^account/reset-password/complete/$',
 )
 	
 EMAIL_HOST = 'localhost'
