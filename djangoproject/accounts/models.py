9c3c86828d4aaaa3068230546360c85e63536810
Sachin Hansda <sachinhansda8@gmail.com>
2019-09-19 20:34:24 +0530

    Display UserProfile Model Info in Templates on Profile Page

--- djangoproject/accounts/models.py
+++ djangoproject/accounts/models.py
@@ -12,6 +12,9 @@ class UserProfile(models.Model):
 	city = models.CharField(max_length=100, default='')
 	phone = models.IntegerField(default=0)
 
+	def __str__(self):
+		return self.user.username
+
 def create_profile(sender, **kwargs):
 	if kwargs['created']:
 		user_profile = UserProfile.objects.create(user=kwargs['instance'])
