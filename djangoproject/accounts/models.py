5540523a96379b30303d3845401fc4006a35b22d
Sachin Hansda <sachinhansda8@gmail.com>
2019-09-21 00:05:14 +0530

    How to Write Your First Custom Model Manager

--- djangoproject/accounts/models.py
+++ djangoproject/accounts/models.py
@@ -6,12 +6,18 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
 # Create your models here.
+class UserProfileManager(models.Manager):
+	def get_queryset(self):
+		return super(UserProfileManager, self).get_queryset().filter(city='Palakkad')
+
 class UserProfile(models.Model):
 	user = models.OneToOneField(User)
 	decription = models.CharField(max_length=100, default='')
 	city = models.CharField(max_length=100, default='')
 	phone = models.IntegerField(default=0)
 	image = models.ImageField(upload_to='profile_image', blank=True)
+
+	palakkad = UserProfileManager()
 
 	def __str__(self):
 		return self.user.username
