c0f5bd88dd8b7140c84deac76a7bc4407700a203
Sachin Hansda <sachinhansda8@gmail.com>
2019-09-29 11:36:37 +0530

    Create a Simple Home Page using Class Based Views

--- djangoproject/home/views.py
+++ djangoproject/home/views.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
-def home(request):
-	return HttpResponse('It works!')
+
+class HomeView(TemplateView):
+	template_name = 'home/home.html'
