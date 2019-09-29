e90661c80951721f6eef0a777fa2d14155dfe7ef
Sachin Hansda <sachinhansda8@gmail.com>
2019-09-29 13:09:27 +0530

    Introduction to Django Forms

---
+++ djangoproject/home/forms.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django import forms
+
+
+class HomeForm(forms.Form):
+	post = forms.CharField()
--- djangoproject/home/views.py
+++ djangoproject/home/views.py
@@ -2,7 +2,14 @@
 from __future__ import unicode_literals
 
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from home.forms import HomeForm
+
 
 
 class HomeView(TemplateView):
 	template_name = 'home/home.html'
+
+	def get(self, request):
+		form = HomeForm()
+		return render(request, self.template_name, {'form': form})
