9d2564542e2c14be94b80d86d645f9cf33949e1f
Sachin Hansda <sachinhansda8@gmail.com>
2019-09-30 18:54:34 +0530

    Pass a Primary Key through a Django URL

--- djangoproject/accounts/views.py
+++ djangoproject/accounts/views.py
@@ -28,8 +28,12 @@ def register(request):
 
 
 
-def view_profile(request):
-	args = {'user': request.user}
+def view_profile(request, pk=None):
+	if pk:
+		user = User.objects.get(pk=pk)
+	else:
+		user = request.user
+	args = {'user': user}
 	return render(request, 'accounts/profile.html', args)
 
 
