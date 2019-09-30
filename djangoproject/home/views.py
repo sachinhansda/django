9ec12a7c5c4a18f85b1fa789789fc5855ba33087
Sachin Hansda <sachinhansda8@gmail.com>
2019-09-30 20:18:37 +0530

    Add a Friend Using a Django View(some error)

--- djangoproject/home/views.py
+++ djangoproject/home/views.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from home.forms import HomeForm
-from home.models import Post
+from home.models import Post, Friend
 
 
 class HomeView(TemplateView):
@@ -31,3 +31,11 @@ class HomeView(TemplateView):
 		
 		args = {'form': form, 'text': text}
 		return render(request, self.template_name, args)
+
+	def change_friends(request, operation, pk):
+		new_friend = User.objects.get(pk=pk)
+		if operation == 'add':
+			Friend.make_friend(request.user, new_friend)
+		elif operation == 'remove':
+			Friend.lose_friend(request.user, new_friend)
+		return redirect('home:home')
