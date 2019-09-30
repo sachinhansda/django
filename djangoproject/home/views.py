7074df742d0954e68f7ce263a6e11431e7990934
Sachin Hansda <sachinhansda8@gmail.com>
2019-09-30 20:36:43 +0530

    Present a Friends List on the Home Page in Django

--- djangoproject/home/views.py
+++ djangoproject/home/views.py
@@ -15,8 +15,10 @@ class HomeView(TemplateView):
 		form = HomeForm()
 		posts = Post.objects.all().order_by('-created')
 		users = User.objects.exclude(id=request.user.id)
+		friend = Friend.objects.get(current_user=request.user)
+		friends = friend.users.all()
 		
-		args = {'form': form, 'posts': posts, 'users': users}
+		args = {'form': form, 'posts': posts, 'users': users, 'friends': friends}
 		return render(request, self.template_name, args)
 
 	def post(self, request):
